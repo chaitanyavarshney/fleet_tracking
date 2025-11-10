b8f6bcd2119ee20f64bb08a6d275ffb82c52c3af
Chaitanya Varshney <chaitanya.varshney@gmail.com>
2025-11-10 10:02:03 +0530

    pipeline reverted + chagnes

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module '*.jsx' {
+    const component: React.ComponentType<any>;
+    export default component;
+  }
+  
+  declare module '*.js' {
+    const component: React.ComponentType<any>;
+    export default component;
+  }
